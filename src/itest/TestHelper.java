da4b5fd8ef0d9f5cbc2a2e988d43a204c2ddb869
Guillaume Turri <guillaume.turri@gmail.com>
2012-12-20 22:18:41 +0100

    Added TestHelper

---
+++ src/itest/TestHelper.java
@@ -0,0 +1,32 @@
+package itest;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import dw.DokuJClient;
+import dw.exception.DokuException;
+import dw.exception.DokuPageLockedException;
+
+public class TestHelper {
+	public static void assertPageIsLockForMe(String pageId, DokuJClient client) throws DokuException{
+		boolean lockExceptionCaught = false;
+		try {
+			client.appendPage(pageId, "something");
+		} catch ( DokuPageLockedException e){
+			lockExceptionCaught = true;
+		}
+		
+		assertTrue(lockExceptionCaught);
+	}
+	
+	public static void assertPageIsUnlockForMe(String pageId, DokuJClient client) throws DokuException{
+		String initialContent = client.getPage(pageId);
+		try {
+			client.appendPage(pageId, "something");
+		} catch (DokuPageLockedException e){
+			fail();
+		}
+		
+		//Clean page content
+		client.putPage(pageId, initialContent);
+	}
+}
