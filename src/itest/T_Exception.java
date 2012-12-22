4c1143fc19e236f7c8292f4fed119892e8049b43
Guillaume Turri <guillaume.turri@gmail.com>
2012-12-22 13:56:49 +0100

    refactored tests

--- src/itest/T_Exception.java
+++ src/itest/T_Exception.java
@@ -1,7 +1,5 @@
 package itest;
 
-import static org.junit.Assert.*;
-
 import java.net.MalformedURLException;
 
 import dw.DokuJClient;
@@ -9,16 +7,12 @@ import dw.exception.DokuPageLockedException;
 import dw.exception.DokuUnauthorizedException;
 
 public class T_Exception {
-	private static String _url = "http://localhost/dokuwikiITestsForXmlRpcClient/lib/exe/xmlrpc.php";
-	private static String _user = "xmlrpcuser";
-	private static String _password = "xmlrpc";
-	
 	private DokuJClient _client;
 	
 
 	@org.junit.Before
 	public void setup() throws MalformedURLException {
-		_client = new DokuJClient(_url, _user, _password);
+		_client = new DokuJClient(TestParams.url, TestParams.user, TestParams.password);
 	}
 	
 	@org.junit.Test(expected=DokuUnauthorizedException.class)
@@ -27,22 +21,12 @@ public class T_Exception {
 		unauthorizedClient.getTime();
 	}
 	
-	@org.junit.Test
+	@org.junit.Test(expected=DokuPageLockedException.class)
 	public void pageLockedException() throws Exception {
 		String pageId = "ns1:start";
 		_client.lock(pageId);
 		
-		DokuJClient otherClient = new DokuJClient(_url, "writeruser", "writer");
-		boolean relevantExceptionCaught = false;
-		try {
-			otherClient.appendPage(pageId, "something");
-		} catch ( DokuPageLockedException e){
-			relevantExceptionCaught = true;
-		}
-		finally {
-			_client.unlock(pageId);	
-		}
-		assertTrue(relevantExceptionCaught);
-		
+		DokuJClient otherClient = new DokuJClient(TestParams.url, TestParams.writerLogin, TestParams.writerPwd);
+		otherClient.appendPage(pageId, "something");
 	}
 }
