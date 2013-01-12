55c340645010fc7a5b3000dbb7ffbc208e53dc9b
Guillaume Turri <guillaume.turri@gmail.com>
2013-01-12 14:20:19 +0100

    Removed a warning

--- src/dw/xmlrpc/itest/T_XmlRpcQueries.java
+++ src/dw/xmlrpc/itest/T_XmlRpcQueries.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
