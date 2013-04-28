20782f14b3f0ecc28953c446fab0944a02e41376
Guillaume Turri <guillaume.turri@gmail.com>
2013-04-28 10:00:07 +0200

    Added a cookies getter

---
+++ src/dw/xmlrpc/itest/T_Cookies.java
@@ -0,0 +1,36 @@
+package dw.xmlrpc.itest;
+
+import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
+import java.util.Collection;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import dw.xmlrpc.DokuJClient;
+import dw.xmlrpc.exception.DokuException;
+
+@RunWith(value = Parameterized.class)
+public class T_Cookies {
+	private TestParams _params;
+
+	public T_Cookies(TestParams params) throws MalformedURLException, DokuException{
+		_params = params;
+	}
+
+	@Parameters
+	 public static Collection<Object[]> data() {
+		 return TestParams.data();
+	 }
+
+	@org.junit.Test
+	public void cookies() throws Exception {
+		DokuJClient client = new DokuJClient(_params.url);
+		assertEquals(0, client.cookies().size());
+		client.login(TestParams.writerLogin, TestParams.writerPwd);
+		assertTrue(client.cookies().size() > 0);
+	}
+
+}
