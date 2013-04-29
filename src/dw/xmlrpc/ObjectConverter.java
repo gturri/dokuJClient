95c6fbb8dccc71b2bc13ff86349530c627b83063
Guillaume Turri <guillaume.turri@gmail.com>
2013-04-29 11:43:41 +0200

    ObjectConverter isn't public anymore
    Hence breaking the public API.
    However, because of its methods visibiliy, it's not worth going though a
    deprecating phrase first.

--- src/dw/xmlrpc/ObjectConverter.java
+++ src/dw/xmlrpc/ObjectConverter.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 //! @cond
-public class ObjectConverter {
+class ObjectConverter {
 
 	static List<PageDW> toPageDW(Object[] objs){
 		List<PageDW> result = new ArrayList<PageDW>();
