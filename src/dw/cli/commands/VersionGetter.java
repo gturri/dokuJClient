7ab542dd24de794454a885da20c3b32db2fc4495
Guillaume Turri <guillaume.turri@gmail.com>
2013-12-01 15:01:59 +0100

    Refactoring (added abstract SimpleCommand)

--- src/dw/cli/commands/VersionGetter.java
+++ src/dw/cli/commands/VersionGetter.java
@@ -1,24 +1,11 @@
 package dw.cli.commands;
 
-import com.martiansoftware.jsap.JSAP;
-import com.martiansoftware.jsap.JSAPException;
-import com.martiansoftware.jsap.JSAPResult;
-
-import dw.cli.Command;
-import dw.cli.Output;
 import dw.xmlrpc.DokuJClient;
 import dw.xmlrpc.exception.DokuException;
 
-public class VersionGetter extends Command {
-
+public class VersionGetter extends SimpleCommand {
 	@Override
-	protected void registerParameters(JSAP jsap) throws JSAPException {	}
-
-	@Override
-	protected Output run(DokuJClient dokuClient, JSAPResult config) throws DokuException {
-		Output output = new Output();
-		output.out = dokuClient.getVersion();
-		return output;
+	protected String run(DokuJClient dokuClient) throws DokuException {
+		return dokuClient.getVersion();
 	}
-
 }
