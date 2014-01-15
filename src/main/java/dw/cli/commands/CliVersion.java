fcf7d8a054ff6ebcfc32c8f2f634bd4b349063ce
Guillaume Turri <guillaume.turri@gmail.com>
2014-01-15 07:30:54 +0100

    Maven can buid a tarball with the exectable

--- src/main/java/dw/cli/commands/CliVersion.java
+++ src/main/java/dw/cli/commands/CliVersion.java
@@ -2,13 +2,34 @@ package dw.cli.commands;
 
 import dw.xmlrpc.DokuJClient;
 import dw.xmlrpc.exception.DokuException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.lang.RuntimeException;
 
 public class CliVersion extends SimpleCommand {
-	//version automatically updated by ant
-	public static final String CliVersion = "3.0.0";
-
 	@Override
 	protected String query(DokuJClient dokuClient) throws DokuException {
-		return CliVersion;
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+			input = CliVersion.class.getResourceAsStream("/application.properties");
+
+			// load a properties file
+			prop.load(input);
+
+			return prop.getProperty("application.version");
+		} catch (IOException e) {
+		    throw new RuntimeException("Can't open property file to read version", e);
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+				  //Shouldn't matter that much
+				}
+			}
+		}
 	}
 }
