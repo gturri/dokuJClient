1ae94d2ee1ee808dfbd267e50e38e5ba0ae8ae89
Guillaume Turri <guillaume.turri@gmail.com>
2014-08-29 19:58:38 +0200

    Added akka flag

--- src/main/java/dw/cli/commands/Syncer.java
+++ src/main/java/dw/cli/commands/Syncer.java
@@ -1,10 +1,20 @@
 package dw.cli.commands;
 
+import akka.util.Timeout;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.Callable;
+
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
+import akka.actor.ActorSystem;
+import static akka.dispatch.Futures.future;
 
 import com.martiansoftware.jsap.FlaggedOption;
 import com.martiansoftware.jsap.JSAP;
@@ -17,16 +27,19 @@ import dw.cli.Program;
 import dw.xmlrpc.DokuJClient;
 import dw.xmlrpc.PageDW;
 import dw.xmlrpc.exception.DokuException;
+import dw.xmlrpc.exception.DokuUnknownException;
 
 public class Syncer extends Command {
 
 	private DokuJClient _dwClientOrigin;
 	private DokuJClient _dwClientDestination;
 	private boolean _verbose;
+	private boolean _useAkka;
 	
 	@Override
 	protected void registerParameters(JSAP jsap) throws JSAPException {
 		jsap.registerParameter(new Switch("verbose").setShortFlag('v').setLongFlag("verbose"));
+		jsap.registerParameter(new Switch("akka").setLongFlag("akka"));
 		jsap.registerParameter(new FlaggedOption("urlDest").setStringParser(JSAP.URL_PARSER).setRequired(true).setLongFlag("urlDest"));
 		jsap.registerParameter(new FlaggedOption("loginDest").setStringParser(JSAP.STRING_PARSER).setRequired(false).setLongFlag("loginDest"));
 		jsap.registerParameter(new FlaggedOption("passwordDest").setStringParser(JSAP.STRING_PARSER).setRequired(false).setLongFlag("passwordDest"));
@@ -37,21 +50,53 @@ public class Syncer extends Command {
 		_dwClientOrigin = dokuClient;
 		_dwClientDestination = Program.buildDokuClient(_config.getURL("urlDest"), _config.getString("loginDest"), _config.getString("passwordDest"));
 		_verbose = _config.getBoolean("verbose");
+		_useAkka = _config.getBoolean("akka");
 		List<PageDW> pagesToPush = getPagesToPush();
-		pushPages(pagesToPush);
+
+		if ( _useAkka ){
+			try {
+				pushPagesWithAkka(pagesToPush);
+			} catch (Exception e) {
+				throw new DokuUnknownException(e);
+			}
+		} else {
+			pushPages(pagesToPush);
+		}
 		return new Output();
+	}
+	
+	private void pushPagesWithAkka(List<PageDW> pagesToPush) throws Exception{
+		ActorSystem system = ActorSystem.create("dw-sync");
+		List<Future<Boolean>> futures = new ArrayList<Future<Boolean>>();
+		
+		for(final PageDW page : pagesToPush){
+			futures.add(future(new Callable<Boolean>(){
+				public Boolean call() throws DokuException{
+					pushPage(page);
+					return true;	
+				}
+			}, system.dispatcher()));
+		}
+		
+		for(Future<Boolean> future : futures){
+			Await.result(future, new Timeout(Duration.create(30, "seconds")).duration());
+		}
 	}
 	
 	private void pushPages(List<PageDW> pagesToPush) throws DokuException {
 		for(PageDW page : pagesToPush){
-			if ( _verbose ){
-				System.out.println("Handling " + page.id() + " ...");
-			}
-			String rawWikiText = _dwClientOrigin.getPage(page.id());
-			_dwClientDestination.putPage(page.id(), rawWikiText);
-			if ( _verbose ){
-				System.out.println("Done with " + page.id());
-			}
+			pushPage(page);
+		}
+	}
+	
+	private void pushPage(PageDW page) throws DokuException {
+		if ( _verbose ){
+			System.out.println("Handling " + page.id() + " ...");
+		}
+		String rawWikiText = _dwClientOrigin.getPage(page.id());
+		_dwClientDestination.putPage(page.id(), rawWikiText);
+		if ( _verbose ){
+			System.out.println("Done with " + page.id());
 		}
 	}
 
