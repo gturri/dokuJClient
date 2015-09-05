25c3b47e48c92eb313e390076395f214064d55f7
Guillaume Turri <guillaume.turri@gmail.com>
2015-09-05 08:41:56 +0200

    More flexibility in build_man.sh
    Can override
    * command used to launch dokujclient
    * destination path

--- man/build_man.sh
+++ man/build_man.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-if ! which dokujclient >/dev/null; then
-  echo dokujclient must be in your PATH in order to run this script
-  exit -1
+if [ -z $DOKUJCLIENT_MAN_PATH ]; then
+  DOKUJCLIENT_MAN_PATH=.
+fi
+
+if [ -z "$DOKUJCLIENT_CMD" ]; then
+  if ! which dokujclient >/dev/null; then
+    echo dokujclient must be in your PATH in order to run this script
+    exit -1
+  fi
+  DOKUJCLIENT_CMD=dokujclient
 fi
 
 rm -rf man1
 mkdir man1
 
-cat man.header.txt > man1/dokujclient.1
-for COMMAND in $(dokujclient --help | grep -v "\[" | grep -v "To get help" | grep -v "Available commands" | grep -v help | sort); do
-  echo .br >> man1/dokujclient.1
-  echo "\fB$COMMAND\fR" >> man1/dokujclient.1
+cat man.header.txt > $DOKUJCLIENT_MAN_PATH/man1/dokujclient.1
+for COMMAND in $($DOKUJCLIENT_CMD --help | grep -v "\[" | grep -v "To get help" | grep -v "Available commands" | grep -v help | sort); do
+  echo .br >> $DOKUJCLIENT_MAN_PATH/man1/dokujclient.1
+  echo "\fB$COMMAND\fR" >> $DOKUJCLIENT_MAN_PATH/man1/dokujclient.1
 done
 
-cat man.footer.txt >> man1/dokujclient.1
+cat man.footer.txt >> $DOKUJCLIENT_MAN_PATH/man1/dokujclient.1
 
